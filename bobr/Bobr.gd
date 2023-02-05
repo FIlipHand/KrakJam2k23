bee4918ff32b87e572d31382a17d1495358c06e3
FIlipHand <filif.reka@gmail.com>
2023-02-05 01:28:20 +0100

    łapanie bobra + zmiana y tree attack

--- bobr/Bobr.gd
+++ bobr/Bobr.gd
@@ -30,17 +30,15 @@ func _ready():
 
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("hit"):
-		if picked_log != null or bubr_state == 1:
-			return
-		bubr_state = BITE
+		if picked_log == null:
+			bubr_state = 1
 	
 	if Input.is_action_just_pressed("pick_drop"):
 		pick_or_drop()
 
 	match bubr_state:
 		MOVE:
-			if(movement_enabled):
-				do_movement()
+			do_movement()
 		BITE:
 			bite_state()
 		
@@ -50,7 +48,7 @@ func do_movement():
 	var input_vec = Vector2.ZERO
 	input_vec.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_vec.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-	input_vec = input_vec.normalized() * MAX_SPEED * (0.6 if picked_log != null else 1.0)
+	input_vec = input_vec.normalized() * MAX_SPEED * (0.6 if picked_log != null else 1.0) * (1.0 if movement_enabled else 0.0001)
 	
 	var x_qt_y = abs(input_vec.x) > abs(input_vec.y)
 	if x_qt_y:
@@ -86,10 +84,13 @@ func do_movement():
 
 func pick_or_drop():
 	if picked_log != null:
-		picked_log.global_position = self.global_position
-		self.rotation_degrees = 0
-		picked_log.show()
-		picked_log = null
+		if movement_enabled:
+			picked_log.global_position = self.global_position
+			picked_log.show()
+			picked_log = null
+		else:
+			picked_log.queue_free()
+			picked_log = null
 	else:
 		if len($PickUpBox.get_overlapping_areas()) > 0:
 			var possible_log_object = $PickUpBox.get_overlapping_areas()[0] # to może generowac błędy
