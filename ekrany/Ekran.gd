e7a9a0ee6604a7881c33e3b11e3c94880cdcc1d0
eroar <48735008+Eroar@users.noreply.github.com>
2023-02-05 11:19:16 +0100

    Add cutscenes and update Ekran.tscn

--- ekrany/Ekran.gd
+++ ekrany/Ekran.gd
@@ -1,6 +1,10 @@
-extends TextureRect
+extends AnimatedSprite
 
 export (PackedScene)var next_scene
+
+func _ready():
+	play()
+
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
