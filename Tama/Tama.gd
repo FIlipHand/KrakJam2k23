f4823e641ebf4e82d8e6a4face13de0e0f563076
eroar <48735008+Eroar@users.noreply.github.com>
2023-02-05 01:55:27 +0100

    Add dynamic Tama building system

--- Tama/Tama.gd
+++ Tama/Tama.gd
@@ -3,29 +3,39 @@ extends Area2D
 signal log_brought(total_logs_count)
 signal tama_built()
 
-export var MAX_FILL: int = 100
+export var MAX_FILL: int = 10
 export var total_log_count: int = 0
 
-var tama_0 = preload('res://Tama/tama_0.png')
-var tama_1 = preload('res://Tama/tama_1.png')
-var tama_2 = preload('res://Tama/tama_2.png')
-var tama_3 = preload('res://Tama/tama_3.png')
+
+var current_stage: int = 0
+var initial_stage = preload('res://Tama/tama_0.png');
+var tama_stages = [
+	preload('res://Tama/tama_1.png'),
+	preload('res://Tama/tama_2.png'),
+	preload('res://Tama/tama_3.png')
+]
 
 func _ready():
-	$Sprite.texture = tama_0
+	$Sprite.texture = initial_stage
 
 
 func add_log_to_tama():
+	if(total_log_count>=MAX_FILL):
+		emit_signal("log_brought", total_log_count)
+		return
+		
 	total_log_count+= 1
-	if total_log_count >= 1 and total_log_count < 3:
-		$Sprite.texture = tama_1
-	if total_log_count >= 3 and total_log_count < 5:
-		$Sprite.texture = tama_2
-	if total_log_count >= 10:
-		$Sprite.texture = tama_3
+	emit_signal("log_brought", total_log_count)
+
+	var logs_per_stage = float(MAX_FILL) / float(len(tama_stages))
+	
+	if(total_log_count >= logs_per_stage * current_stage && total_log_count<MAX_FILL):
+		print("totalcount:", total_log_count, "logs_per_stage", logs_per_stage)
+		current_stage += 1
+		$Sprite.texture = tama_stages[current_stage - 1]
+
 	if total_log_count >= MAX_FILL:
 		emit_signal("tama_built")
-	emit_signal("log_brought", total_log_count)
 
 
 func _on_Node2D_area_entered(area: Area2D):
