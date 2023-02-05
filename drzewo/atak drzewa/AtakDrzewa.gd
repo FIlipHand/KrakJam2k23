e720e4a6aaeed81615875984b4abbe78deec2894
eroar <48735008+Eroar@users.noreply.github.com>
2023-02-05 10:45:24 +0100

    Change AtakDrzewa to version 2.0

--- drzewo/atak drzewa/AtakDrzewa.gd
+++ drzewo/atak drzewa/AtakDrzewa.gd
@@ -2,13 +2,13 @@ extends Area2D
 
 export var HP:int = 2
 
-onready var frames_count = $AnimatedSprite.frames.get_frame_count("default")
 var player = null
 
 func _ready():
 	$AnimatedSprite.play()
 
-func attack():
+func catch():
+	$AnimatedSprite.animation = "catch"
 	var caught:bool = false
 	for body in get_overlapping_bodies():
 		if(body.is_in_group("player")):
@@ -18,24 +18,22 @@ func attack():
 	if caught:
 		_on_caught()
 	else:
-		queue_free()
+		_on_failed_catch()
 
+func _on_failed_catch():
+	$AnimatedSprite.animation = "failed catch"
+	$AnimatedSprite.frame = 0
 
 # jeśli złapie coś rootsami
 func _on_caught():
 	# sets the last frame where it is caught
+	HP += 1
 	$AnimatedSprite.stop()
-	$AnimatedSprite.frame = frames_count-1 #$AnimatedSprite.frames.get_frame("default", frames_count-1)
-
 
 func _on_player_caught():
 	if player != null:
 		player.movement_enabled = false
 
-
-func _on_AnimatedSprite_frame_changed():
-	if ($AnimatedSprite.frame == frames_count-1):
-		attack()
 
 func _on_attacked():
 	HP -= 1
@@ -45,3 +43,16 @@ func _on_attacked():
 			player.movement_enabled = true
 		hide()
 		queue_free()
+
+
+func _on_AtakDrzewa_body_entered(body:Node):
+	$AnimatedSprite.play("catch")
+
+func _on_AnimatedSprite_animation_finished():
+	if $AnimatedSprite.animation == "catch":
+		catch()
+
+	elif ($AnimatedSprite.animation == "failed catch"):
+		$AnimatedSprite.play("default")
+		# hide()
+		# queue_free()
